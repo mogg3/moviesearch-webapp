fdd692f32f3264ee97461ec0017fd2f163569873
Oskar H <oskar.hallin@gmail.com>
2020-12-22 14:06:10 +0100

    structures the project in right folders and adds pages and login pseudo-function

---
+++ view/__init__.py
@@ -0,0 +1,8 @@
+from flask import Flask
+
+
+app = Flask(__name__)
+app.secret_key = 'super secret key'
+
+import view.html_routes
+import view.api_routes
---
+++ view/api_routes.py
@@ -0,0 +1,14 @@
+from flask import request
+
+#
+# def get_movie_by_title(input_title):
+#     parameters = {'t': input_title, 'r': 'json', 'plot': 'short'}
+#     req = request.get("http://www.omdbapi.com/?apikey=6f91aff9", params=parameters)
+#
+#     result = req.json()
+#
+#     print(result)
+#
+#
+# get_movie_by_title("Interstellar")
+#
