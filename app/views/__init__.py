2f89bb9b7986db11db7ec6e8505c4060d030d991
Marcus Eriksson <marcus.eriksson00@hotmail.com>
2021-01-12 13:23:17 +0100

    test

--- app/views/__init__.py
+++ app/views/__init__.py
@@ -1,8 +1,6 @@
-from flask import Flask
+def main():
+    pass
 
 
-app = Flask(__name__)
-app.secret_key = 'super secret key'
-
-import views.html_routes
-import views.api_routes
+if __name__ == "__main__":
+    main()
--- app/views/api_routes.py
+++ app/views/api_routes.py
@@ -5,4 +5,5 @@ from views.api_settings import API_KEY
 def get_movie_by_title(input_title):
     parameters = {'s': input_title, 'r': 'json', 'plot': 'short'}
     req = requests.get(f'http://www.omdbapi.com/?apikey={API_KEY}', params=parameters)
+    print(req.json())
     return req.json()
