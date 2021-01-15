ba2f02d9047d98d78efb3376954ef2c84c94fae5
Marcus Eriksson <marcus.eriksson00@hotmail.com>
2021-01-15 08:43:20 +0100

    small fixes

--- app/views/api_routes.py
+++ app/views/api_routes.py
@@ -1,11 +1,9 @@
 import requests
-
 from views.api_settings import API_KEY
 
 
 def get_movie_by_title_first(input_title):
     parameters = {'t': input_title, 'r': 'json', 'plot': 'short'}
-
     req = requests.get(f'http://www.omdbapi.com/?apikey={API_KEY}', params=parameters)
 
     return req.json()
