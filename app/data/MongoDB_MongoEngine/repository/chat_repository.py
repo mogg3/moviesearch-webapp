9a872f77164e06c79dbae6afc5d291388910b7f2
Oskar H <oskar.hallin@gmail.com>
2021-01-20 09:44:08 +0100

    together with Marcus, continuing on chat

--- app/data/MongoDB_MongoEngine/repository/chat_repository.py
+++ app/data/MongoDB_MongoEngine/repository/chat_repository.py
@@ -1,13 +1,8 @@
 from data.MongoDB_MongoEngine.models.chats import Chat
-from data.MongoDB_MongoEngine.repository.message_repository import create_message
-from data.MongoDB_MongoEngine.repository.user_repository import get_user_by_email
+from data.MongoDB_MongoEngine.repository.message_repository import send_message
 
 
-def initiate_chat():
-    user1 = get_user_by_email("q@m2123")
-    user2 = get_user_by_email("m@m2123")
-    message = create_message()
-    chat = Chat(members=[user1, user2], messages=[message])
+def initiate_chat(user_1, user_2):
+    chat = Chat(members=[user_1, user_2], messages=[send_message()])
     chat.save()
-    return chat
-
+    print(chat)
