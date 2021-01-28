8640af47e86c74b760125da85adde56d752a0932
Oskar H <oskar.hallin@gmail.com>
2021-01-28 18:33:06 +0100

    updating requirements

--- app/views/html_routes.py
+++ app/views/html_routes.py
@@ -48,7 +48,6 @@ def sign_up():
     return render_template('signup.html', form=form)
 
 
-
 @app.route("/signout")
 @login_required
 def sign_out():
@@ -65,22 +64,21 @@ def profile():
 @app.route('/profile', methods=['POST'])
 @login_required
 def post_profile_picture():
-    if request.method == "POST":
-        if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
+    if 'file' not in request.files:
+        flash('No file part')
+        return redirect(request.url)
 
-        file = request.files['file']
+    file = request.files['file']
 
-        if not file.filename:
-            flash('No selected file')
-            return redirect(request.url)
+    if not file.filename:
+        flash('No selected file')
+        return redirect(request.url)
 
-        if file.filename.rsplit('.', 1)[1].lower() in {'png', 'jpg', 'jpeg', 'gif'}:
-            delete_profile_picture_if_exists(current_user)
-            add_profile_picture_to_user(current_user, file)
-        else:
-            flash('Wrong file format. Choose between png, jpg, jpeg and gif.')
+    if file.filename.rsplit('.', 1)[1].lower() in {'png', 'jpg', 'jpeg', 'gif'}:
+        delete_profile_picture_if_exists(current_user)
+        add_profile_picture_to_user(current_user, file)
+    else:
+        flash('Wrong file format. Choose between png, jpg, jpeg and gif.')
 
     return redirect(url_for('profile'))
 
