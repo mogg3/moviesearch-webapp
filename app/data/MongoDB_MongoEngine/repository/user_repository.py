bfa24306b86ba9457c654e325d6ace211923a6e7
Marcus Eriksson <marcus.eriksson00@hotmail.com>
2021-02-01 11:03:55 +0100

    responsive friends-list

--- app/data/MongoDB_MongoEngine/repository/user_repository.py
+++ app/data/MongoDB_MongoEngine/repository/user_repository.py
@@ -3,6 +3,11 @@ from flask_security.utils import hash_password
 from data.MongoDB_MongoEngine.models.users import User
 from data.MongoDB_MongoEngine.db import user_datastore
 
+def clean_database():
+    for user in User.objects:
+        user.friends=[]
+        user.chats=[]
+        user.save()
 
 def create_user(first_name: str, last_name: str, email: str, password: str, username: str):
     user_datastore.create_user(
